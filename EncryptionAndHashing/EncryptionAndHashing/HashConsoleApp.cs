6c9d5e0bf0bbbbb0d546dd39b98e969f3236a0fb
unknown <faezehjafarpour75@gmail.com>
2022-06-12 11:28:57 +0430

    hash

---
+++ EncryptionAndHashing/EncryptionAndHashing/HashConsoleApp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionAndHashing
+{
+    public static class HashConsoleApp
+    {
+       public static string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        //public string Getsha256Hash(string value)
+        //{
+        //    var algoritm = new SHA256CryptoServiceProvider();
+        //    var byteValue = Encoding.UTF8.GetBytes(value);
+        //    var byteHash = algoritm.ComputeHash(byteValue);
+        //    return Convert.ToBase64String(byteHash);
+        //}
+    }
+}
---
+++ EncryptionAndHashing/EncryptionAndHashing/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//Encryption
+using EncryptionAndHashing;
+
+var key = "b14ca5898a4e4133bbce2ea2315a1916";
+
+//Console.WriteLine("Please enter a secret key for the symmetric algorithm.");  
+//var key = Console.ReadLine();  
+
+Console.WriteLine("Please enter a string for encryption");
+var str = Console.ReadLine();
+var encryptedString = AesOperation.EncryptString(key, str);
+Console.WriteLine($"encrypted string = {encryptedString}");
+
+var decryptedString = AesOperation.DecryptString(key, encryptedString);
+Console.WriteLine(decryptedString);
+
+//
+//Console.WriteLine("Please enter a password to use:");
+//string password = Console.ReadLine();
+//Console.WriteLine("Please enter a string to encrypt:");
+//string plaintext = Console.ReadLine();
+//Console.WriteLine("");
+
+//Console.WriteLine("Your encrypted string is:");
+//string encryptedstring = EncryptStringSample.Encrypt(plaintext, password);
+//Console.WriteLine(encryptedstring);
+//Console.WriteLine("");
+
+//Console.WriteLine("Your decrypted string is:");
+//string decryptedstring = EncryptStringSample.Decrypt(encryptedstring, password);
+//Console.WriteLine(decryptedstring);
+//Console.WriteLine("");
+
+//hash
+string plainData = "faezeh";
+Console.WriteLine("Raw data: {0}", plainData);
+string hashedData = HashConsoleApp.ComputeSha256Hash(plainData);
+Console.WriteLine("Hash {0}", hashedData);
+Console.WriteLine(HashConsoleApp.ComputeSha256Hash("faezeh"));
+
+Console.ReadKey();
